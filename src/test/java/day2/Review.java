62f2941db2a4795d08db329cf14934c15f024500
sarabhadi <hadisarab007@gmail.com>
2020-11-26 16:18:35 -0600

    Day new 2

--- src/test/java/day2/Review.java
+++ src/test/java/day2/Review.java
@@ -1,47 +1,52 @@
 package day2;
 
+import utility.DB_Utility;
+
 import java.sql.*;
 
 public class Review {
+
     public static void main(String[] args) throws SQLException {
-        String connectionStr = "jdbc:oracle:thin:@54.157.171.44:1521:XE";
-        String username = "hr" ;
-        String password = "hr" ;
-        Connection conn = DriverManager.getConnection(connectionStr,username,password) ;
-        Statement stmnt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-        ResultSet rs   =   stmnt.executeQuery("SELECT * FROM JOBS") ;
-        rs.next(); // currently we are at the very first row
-        //
+
+        DB_Utility.createConnection();
+        ResultSet rs = DB_Utility.runQuery("SELECT * FROM JOBS");
+
+        // I want to read the first row
         rs.next();
-        System.out.println("First column value in jobs " +rs.getString(1));
-        System.out.println("Second column value in jobs " +rs.getString(2));
-        //
+        System.out.println("First column value in Jobs " + rs.getString(1));
+        System.out.println("Second column value in Jobs " + rs.getString(2));
+
+        // move to row number 7 and get above 2 columns values
         rs.absolute(7);
-        System.out.println("First column value in Row 7 " +rs.getString(1));
-        System.out.println("Second column value in Row 7 " +rs.getString(2));
+        System.out.println("First column value in Jobs in row 7 " + rs.getString(1));
+        System.out.println("Second column value in Jobs in row 7 " + rs.getString(2));
+
         rs.last();
-        System.out.println("First column value in last row " + rs.getString(1));
-        System.out.println("Second column value in last row " + rs.getString(2));
+        System.out.println("First column value in Jobs in last row " + rs.getString(1));
+        System.out.println("Second column value in Jobs in last row " + rs.getString(2));
 
         rs.previous();
-        System.out.println("First column value in 2nd row from last " + rs.getString(1));
-        System.out.println("Second column value in 2nd row from last " + rs.getString(2));
+        System.out.println("First column value in Jobs in 2nd row from last  " + rs.getString(1));
+        System.out.println("Second column value in Jobs in 2nd row from last  " + rs.getString(2));
 
-        System.out.println("--------Loop from last row till first row get MIN_SALARY COLUMN AS NUMBER--------");
+        System.out.println("\n---------Loop from first row till last row print JOB_ID-------");
+        rs.beforeFirst();
 
-       // rs.beforeFirst();
-        //while( rs.next()){
-           // System.out.println("Loop First Column " +rs.getString("JOB_ID"));
-        rs.afterLast();
-        while( rs.previous()){
-            System.out.println("MIN SALARY COLUMN AS NUMBER $ " +rs.getDouble("MIN_SALARY"));
+        while (rs.next()) {
+            System.out.println("Loop First Column " + rs.getString("JOB_ID"));
         }
-        // clean up the connection, result, and statement
-        rs.close();
-        stmnt.close();
-        conn.close();
 
+        System.out.println("\n---------Loop from last row till first row get MIN_SALARY COLUMN AS NUMBER-------");
+        // WE ARE CURRENTLY AT AFTER LAST LOCATION
+        // if you really want to make sure and explicitly say so
+        // we can do below
+        rs.afterLast();
+        while (rs.previous()) {
+            // NOTHING WRONG WITH GETTING IT AS STRING , JUST FOR THE SAKE OF DEMO , WE ARE GETTING AS DOUBLE
+            System.out.println("MIN SALARY COLUMN AS NUMBER  $" + rs.getDouble("MIN_SALARY"));
+        }
 
-
+        // clean up the connection , statement and resultset object after usage
+        DB_Utility.destroy();
     }
 }
